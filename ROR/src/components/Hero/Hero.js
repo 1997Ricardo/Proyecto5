81a495c330e7e216c38c13ba55010cfd4fb5c1d3
1997Ricardo <ricardosunarodriguez@gmail.com>
2025-06-10 08:27:33 +0200

    variación de estilos

--- ROR/src/components/Hero/Hero.js
+++ ROR/src/components/Hero/Hero.js
@@ -1,11 +1,16 @@
 import './Hero.css';
 
 const Hero = () => `
-  <section class="hero">
-    <h2>Bienvenido a ROR</h2>
-    <p>Maximiza tu rendimiento con nuestros programas de readaptación y optimización.</p>
-    <a href="#services" class="hero-button">Descubre más</a>
-  </section>
+<section class="hero">
+  <div class="hero-inner">
+    <img class="hero-image" src="/public/assets/hero.png" alt="Foto de perfil" />
+    <div class="hero-text">
+      <h2>Bienvenido a ROR</h2>
+      <p>Maximiza tu rendimiento con nuestros programas de readaptación y optimización.</p>
+      <a href="#services" class="hero-button">Descubre más</a>
+    </div>
+  </div>
+</section>
 `;
 
 export default Hero;
