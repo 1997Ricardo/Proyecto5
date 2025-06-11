7de7fb02146151c2426002633c020feef0393a22
1997Ricardo <ricardosunarodriguez@gmail.com>
2025-06-11 17:42:43 +0200

    Update Nav.js

--- ROR/src/components/NavBar/Nav.js
+++ ROR/src/components/NavBar/Nav.js
@@ -4,6 +4,7 @@ import './Nav.css';
 const Nav = () => `
     <nav class="app-nav">
   <ul>
+    <li><a href="#hero">Inicio</a></li>
     <li><a href="#services">Servicios</a></li>
     <li><a href="#contact">Contacto</a></li>
     <li><a href="#about">Sobre mí</a></li>
