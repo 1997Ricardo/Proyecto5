22bf0a5ef3315aa304c54f487a5e5b81a84a3926
1997Ricardo <ricardosunarodriguez@gmail.com>
2025-06-11 16:21:51 +0200

    Update Header.js

--- ROR/src/components/Header/Header.js
+++ ROR/src/components/Header/Header.js
@@ -4,7 +4,7 @@ import './Header.css';
 const Header = () => `
   <header>
     <div class="header-inner">
-      <img class="header-logo" src="./public/assets/logo.png" alt="ROR Logo" />
+      <img class="header-logo" src="/assets/logo.png" alt="ROR Logo" />
       <div class="header-text">
         <h1>ROR</h1>
         <p>Readaptación, Optimización y Rendimiento</p>
